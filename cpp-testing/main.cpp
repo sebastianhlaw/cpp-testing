9e426aba6000e139398745822a090894edafb28e
sebastianhlaw <sebastianhlaw@gmail.com>
2017-04-11 11:57:36 +0100

    Initial Commit

---
+++ cpp-testing/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cpp-testing
+//
+//  Created by Sebastian Law on 11/04/2017.
+//  Copyright © 2017 Sebastian Law. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
